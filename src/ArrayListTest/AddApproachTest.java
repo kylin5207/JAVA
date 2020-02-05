20d8d7cf133cc6d0de281e95c2dc621db2b16e38
kylin <kylin.smq@qq.com>
2020-02-05 12:13:46 +0800

    ArrayList Class Test for datatype-String

---
+++ src/ArrayListTest/AddApproachTest.java
@@ -0,0 +1,17 @@
+package ArrayListTest;
+
+import java.util.ArrayList;
+
+public class AddApproachTest {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Kylin");
+        list.add("Brush");
+        list.add("Ysl");
+
+        System.out.println(list);
+    }
+}
