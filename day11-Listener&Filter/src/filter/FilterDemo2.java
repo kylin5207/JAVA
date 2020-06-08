811672114e92c6e6eefaa8640ee4a19a5deebe58
kylin <kylin.smq@qq.com>
2020-06-08 13:22:07 +0800

    过滤器执行流程

--- day11-Listener&Filter/src/filter/FilterDemo2.java
+++ day11-Listener&Filter/src/filter/FilterDemo2.java
@@ -4,7 +4,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter("/*")
+//gi@WebFilter("/*")
 public class FilterDemo2 implements Filter {
     public void destroy() {
     }
