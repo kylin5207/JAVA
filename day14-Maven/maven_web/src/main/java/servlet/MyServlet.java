cd59fc394ab104b08c5f0123c0d1b89136b50fdf
kylin <kylin.smq@qq.com>
2020-06-22 16:36:25 +0800

    maven web项目案例

---
+++ day14-Maven/maven_web/src/main/java/servlet/MyServlet.java
@@ -0,0 +1,18 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Hello, Maven");
+        request.getRequestDispatcher("/Hello.jsp").forward(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
