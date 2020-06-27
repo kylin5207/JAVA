caf7b85dc2afea8522acd09160ed5b65ade19fd7
kylin <kylin.smq@qq.com>
2020-06-27 22:41:07 +0800

    激活码与用户状态的设置

--- cases/travel/src/main/java/cn/itcast/travel/service/impl/UserServiceImpl.java
+++ cases/travel/src/main/java/cn/itcast/travel/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import cn.itcast.travel.dao.UserDao;
 import cn.itcast.travel.dao.impl.UserDaoImpl;
 import cn.itcast.travel.domain.User;
 import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.util.MailUtils;
+import cn.itcast.travel.util.UuidUtil;
 
 public class UserServiceImpl implements UserService {
     private UserDao userDao = new UserDaoImpl();
@@ -23,10 +25,19 @@ public class UserServiceImpl implements UserService {
             //用户名存在，注册失败
             return false;
         }
-        else{
-            //用户名不存在，注册成功
-            userDao.save(user);
-            return true;
-        }
+
+        //用户名不存在，注册成功
+        //2.1 设置激活码，唯一字符串
+        user.setCode(UuidUtil.getUuid());
+
+        //2.2 设置激活状态
+        user.setStatus("N");
+        userDao.save(user);
+
+        //邮件发送，发送激活邮件
+        String content = "<a href='http://localhost/activeUserServlet?code="+user.getCode()+"'>点击激活【来自帅琦的问候】</a>";
+        MailUtils.sendMail(user.getEmail(), content, "激活邮件");
+
+        return true;
     }
 }
