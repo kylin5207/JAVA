64fc89590f5e383f6cb328c47fcf1ebedb369cb0
kylin <kylin.smq@qq.com>
2020-08-21 16:50:21 +0800

    计算时变的太阳黄经

--- cases/satelliteAngleCalculation/src/test/java/SolarTermsTest.java
+++ cases/satelliteAngleCalculation/src/test/java/SolarTermsTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 import sun.SolarTerms;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -16,9 +18,33 @@ public class SolarTermsTest {
     }
 
     @Test
-    public void test2(){
-        int i = 12;
-        System.out.println(String.format("%02d", i));
+    public void test2() throws ParseException {
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        String currentDateStr = df.format(date);
+
+        //将年份日期存储
+        int currentYear = Integer.parseInt(currentDateStr.substring(0, 4));
+        int nextYear = currentYear + 1;
+
+        //计算当前年份的春分和下一个春分日
+        String currentDay = SolarTerms.getSolarTermNum(currentYear, "CHUNFEN");
+        String nextDay = SolarTerms.getSolarTermNum(nextYear, "CHUNFEN");
+
+        Date currentCFDay = df.parse(String.valueOf(currentYear) + "-03-" +  currentDay);
+        Date nextCFDay = df.parse(String.valueOf(nextYear) + "-03-" +  nextDay);
+
+        System.out.println(currentCFDay);
+        System.out.println(nextCFDay);
+        long days = (nextCFDay.getTime() - currentCFDay.getTime()) / 1000 / 60 / 60 / 24;
+        long passDays = (date.getTime() - currentCFDay.getTime()) / 1000 / 60 / 60 / 24;
+
+        System.out.println(days);
+        System.out.println(passDays);
+
+        System.out.println(1.0 * 360 / days * passDays);
 
     }
 }
+
+
