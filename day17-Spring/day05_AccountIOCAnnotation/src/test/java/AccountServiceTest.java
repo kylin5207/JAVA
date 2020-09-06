bf0836dfbef4fb0e22bab721c9db51b4b8e5b5ad
kylin <kylin.smq@qq.com>
2020-09-06 18:03:14 +0800

    Spring整合JUnit

--- day17-Spring/day05_AccountIOCAnnotation/src/test/java/AccountServiceTest.java
+++ day17-Spring/day05_AccountIOCAnnotation/src/test/java/AccountServiceTest.java
@@ -1,28 +1,39 @@
 import config.JdbcConfig;
 import config.SpringConfiguration;
 import domain.Account;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import service.IAccountService;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 /**
  * 使用JUnit进行单元测试
  *  每个测试方法都重新获取了一次spring的核心容器，造成了不必要的重复
- *
  *  emmm还是存在缺点的，这种方式虽然能解决问题，但是扔需要我们自己写代码来获取容器。
+ *
+ *  Spring整合junit的配置：
+ *      1. 导入Spring整合junit的jar（坐标）
+ *      2、使用Junit提供的一个注解把原有的main方法替换了，替换成spring提供的  @Runwith
+ *      3、告知spring的运行器，spring和ioc创建是基于xml还是注解的，并且说明位置
+ *      @ContextConfiguration
+ *          locations：指定xml文件的位置，加上classpath关键字，表示在类路径下
+ *          classes：指定注解类所在的位置
+ *
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfiguration.class)
 public class AccountServiceTest {
-    //1. 获取容器
-    //使用AnnotationConfigApplicationContext可不写@Configuration注解
-    ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-//    ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
 
-    //2. 得到业务层对象
-    IAccountService service = ac.getBean("accountService", IAccountService.class);
+    @Autowired
+    private IAccountService service;
 
     @Test
     public void testFindAll(){
